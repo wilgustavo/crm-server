1cb48a12ef5226f9b4c5ebb505d40a64cad359bb
Gustavo Wilgenhoff <wilgustavo@gmail.com>
2020-02-14 19:19:05 -0300

    Rename MainApp

--- src/main/java/com/gmw/MainApp.java
+++ src/main/java/com/gmw/MainApp.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CrmApplication {
+public class MainApp {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CrmApplication.class, args);
+		SpringApplication.run(MainApp.class, args);
 	}
 
 }
